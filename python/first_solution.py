feea7e95cf6a7b52f73f01c3c9eb3c47f39cf34b
Mike Moran <mike@houseofmoran.com>
2021-04-18 16:08:06 +0100

    WIP

--- python/first_solution.py
+++ python/first_solution.py
@@ -1,13 +1,22 @@
 from minizinc import Instance, Model, Solver
+import pprint
 
 # Load n-Queens model from file
 nqueens = Model("./nqueens.mzn")
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(nqueens["n"])
+
 # Find the MiniZinc solver configuration for Gecode
 gecode = Solver.lookup("gecode")
 # Create an Instance of the n-Queens model for Gecode
 instance = Instance(gecode, nqueens)
+
 # Assign 4 to n
 instance["n"] = 4
+
+pp.pprint(instance["n"])
+
 result = instance.solve()
 # Output the array q
 print(result["q"])
