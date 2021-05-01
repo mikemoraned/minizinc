5f1a0ef77b2701a19437a7a537e0073384c200fb
Mike Moran <mike@houseofmoran.com>
2021-05-01 14:06:54 +0100

    extract `graphql_server`

--- tracer/graphql_minizinc_server/src/main.rs
+++ tracer/graphql_minizinc_server/src/main.rs
@@ -7,6 +7,7 @@ use async_graphql::{EmptyMutation, EmptySubscription, Schema};
 use async_graphql_actix_web::{Request, Response};
 use async_graphql::extensions::ApolloTracing;
 use async_graphql::*;
+use actix_web::dev::Server;
 
 mod minizinc;
 mod graphql;
@@ -81,6 +82,12 @@ async fn main() -> std::io::Result<()> {
     print!("model:\n{}", &model_string);
     print!("parsed:\n{:#?}", model(&model_string));
 
+    let server = graphql_server();
+
+    server?.await
+}
+
+pub fn graphql_server() -> Result<Server, std::io::Error> {
     let schema = Schema::build(Query, EmptyMutation, EmptySubscription)
         .extension(ApolloTracing)
         .finish();
@@ -89,13 +96,12 @@ async fn main() -> std::io::Result<()> {
 
     println!("Playground: http://localhost:8000");
 
-    HttpServer::new(move || {
+    Ok(HttpServer::new(move || {
         App::new()
             .data(schema.clone())
             .service(web::resource("/").guard(guard::Post()).to(index))
             .service(web::resource("/").guard(guard::Get()).to(index_playground))
     })
         .bind("127.0.0.1:8000")?
-        .run()
-        .await
+        .run())
 }
