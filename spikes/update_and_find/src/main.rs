6e62a9efd616c027fef22ec34a50dd8ba80196a9
Mike Moran <mike@houseofmoran.com>
2021-07-01 12:59:57 +0100

    redis: `can_set_state`

--- spikes/update_and_find/src/main.rs
+++ spikes/update_and_find/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use testcontainers::{clients, images, Docker};
-    use redis::Commands;
+    use redis::{Commands, RedisResult};
 
     #[tokio::test]
     async fn no_states_by_default() {
@@ -21,5 +21,27 @@ mod tests {
 
         let size: u128 = con.hlen("states").unwrap();
         assert_eq!(0, size);
+    }
+
+    #[tokio::test]
+    async fn can_set_state() {
+        let _ = pretty_env_logger::try_init();
+        let docker = clients::Cli::default();
+        let node = docker.run(images::redis::Redis::default());
+
+        let host_port = node.get_host_port(6379).unwrap();
+        let url = format!("redis://localhost:{}", host_port);
+
+        let client = redis::Client::open(url.as_ref()).unwrap();
+        let mut con = client.get_connection().unwrap();
+
+        let initial_state : RedisResult<String> = con.hget("states", "some_id");
+        assert!(initial_state.is_err());
+
+        let expected_state = "some_state".to_string();
+        con.hset::<&str, &str, &String, u8>("states", "some_id", &expected_state).unwrap();
+
+        let current_state : String = con.hget("states", "some_id").unwrap();
+        assert_eq!(expected_state, current_state);
     }
 }
