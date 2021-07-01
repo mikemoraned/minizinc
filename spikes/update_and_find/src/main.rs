28413087507f0332c380dcf5ab1ea266529f41e6
Mike Moran <mike@houseofmoran.com>
2021-07-01 13:04:38 +0100

    redis: `can_update_state`

--- spikes/update_and_find/src/main.rs
+++ spikes/update_and_find/src/main.rs
@@ -44,4 +44,32 @@ mod tests {
         let current_state : String = con.hget("states", "some_id").unwrap();
         assert_eq!(expected_state, current_state);
     }
+
+    #[tokio::test]
+    async fn can_update_state() {
+        let _ = pretty_env_logger::try_init();
+        let docker = clients::Cli::default();
+        let node = docker.run(images::redis::Redis::default());
+
+        let host_port = node.get_host_port(6379).unwrap();
+        let url = format!("redis://localhost:{}", host_port);
+
+        let client = redis::Client::open(url.as_ref()).unwrap();
+        let mut con = client.get_connection().unwrap();
+
+        let initial_state : RedisResult<String> = con.hget("states", "some_id");
+        assert!(initial_state.is_err());
+
+        let intermediate_state = "some_other_state".to_string();
+        con.hset::<&str, &str, &String, u8>("states", "some_id", &intermediate_state).unwrap();
+
+        let mut current_state : String = con.hget("states", "some_id").unwrap();
+        assert_eq!(intermediate_state, current_state);
+
+        let expected_state = "some_state".to_string();
+        con.hset::<&str, &str, &String, u8>("states", "some_id", &expected_state).unwrap();
+
+        current_state = con.hget("states", "some_id").unwrap();
+        assert_eq!(expected_state, current_state);
+    }
 }
