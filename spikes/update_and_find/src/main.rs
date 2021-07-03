a4fdc69621d91bf182b53150f5eec58a5acb81fc
Mike Moran <mike@houseofmoran.com>
2021-07-03 14:06:54 +0100

    - extract `set_state`, `find_state`

--- spikes/update_and_find/src/main.rs
+++ spikes/update_and_find/src/main.rs
@@ -13,9 +13,25 @@ impl Client {
         Client { redis_client }
     }
 
-    fn find_all_states(self) -> HashSet<(String, String)> {
-        use redis::{Commands, Iter};
+    fn set_state(&self, id: &String, state: &String) {
+        use redis::Commands;
+        let mut con = self.redis_client.get_connection().unwrap();
 
+        con.hset::<&str, &String, &String, u8>("states", id, state).unwrap();
+    }
+
+    fn find_state(&self, id: &String) -> Option<String> {
+        use redis::Commands;
+        let mut con = self.redis_client.get_connection().unwrap();
+
+        match con.hget("states", id)  {
+            Ok(state) => Some(state),
+            Err(_) => None
+        }
+    }
+
+    fn find_all_states(&self) -> HashSet<(String, String)> {
+        use redis::{Commands, Iter};
         let mut con = self.redis_client.get_connection().unwrap();
 
         let states_iter: Iter<'_, (String, String)>= con.hscan("states").unwrap();
@@ -26,7 +42,6 @@ impl Client {
 #[cfg(test)]
 mod tests {
     use testcontainers::{clients, images, Docker};
-    use redis::{Commands, RedisResult};
     use std::collections::HashSet;
     use crate::Client;
 
@@ -54,17 +69,17 @@ mod tests {
         let host_port = node.get_host_port(6379).unwrap();
         let url = format!("redis://localhost:{}", host_port);
 
-        let client = redis::Client::open(url.as_ref()).unwrap();
-        let mut con = client.get_connection().unwrap();
+        let redis_client = redis::Client::open(url.as_ref()).unwrap();
+        let client = Client::new(redis_client);
 
-        let initial_state : RedisResult<String> = con.hget("states", "some_id");
-        assert!(initial_state.is_err());
+        let initial_state  = client.find_state(&"some_id".to_string());
+        assert_eq!(None, initial_state);
 
         let expected_state = "some_state".to_string();
-        con.hset::<&str, &str, &String, u8>("states", "some_id", &expected_state).unwrap();
+        client.set_state(&"some_id".to_string(), &expected_state);
 
-        let current_state : String = con.hget("states", "some_id").unwrap();
-        assert_eq!(expected_state, current_state);
+        let current_state = client.find_state(&"some_id".to_string());
+        assert_eq!(Some(expected_state), current_state);
     }
 
     #[tokio::test]
@@ -76,22 +91,22 @@ mod tests {
         let host_port = node.get_host_port(6379).unwrap();
         let url = format!("redis://localhost:{}", host_port);
 
-        let client = redis::Client::open(url.as_ref()).unwrap();
-        let mut con = client.get_connection().unwrap();
+        let redis_client = redis::Client::open(url.as_ref()).unwrap();
+        let client = Client::new(redis_client);
 
-        let initial_state : RedisResult<String> = con.hget("states", "some_id");
-        assert!(initial_state.is_err());
+        let initial_state  = client.find_state(&"some_id".to_string());
+        assert_eq!(None, initial_state);
 
         let intermediate_state = "some_other_state".to_string();
-        con.hset::<&str, &str, &String, u8>("states", "some_id", &intermediate_state).unwrap();
+        client.set_state(&"some_id".to_string(), &intermediate_state);
 
-        let mut current_state : String = con.hget("states", "some_id").unwrap();
+        let mut current_state = client.find_state(&"some_id".to_string()).unwrap();
         assert_eq!(intermediate_state, current_state);
 
         let expected_state = "some_state".to_string();
-        con.hset::<&str, &str, &String, u8>("states", "some_id", &expected_state).unwrap();
+        client.set_state(&"some_id".to_string(), &expected_state);
 
-        current_state = con.hget("states", "some_id").unwrap();
+        current_state = client.find_state(&"some_id".to_string()).unwrap();
         assert_eq!(expected_state, current_state);
     }
 
@@ -105,17 +120,16 @@ mod tests {
         let url = format!("redis://localhost:{}", host_port);
 
         let redis_client = redis::Client::open(url.as_ref()).unwrap();
-        let mut con = redis_client.get_connection().unwrap();
+        let client = Client::new(redis_client);
 
-        con.hset::<&str, &str, &str, u8>("states", "id1", "state1").unwrap();
-        con.hset::<&str, &str, &str, u8>("states", "id2", "state2").unwrap();
+        client.set_state(&"id1".to_string(), &"state1".to_string());
+        client.set_state(&"id2".to_string(), &"state2".to_string());
 
         let expected_states : HashSet<(String, String)>
             = vec![
                 ("id1".to_string(), "state1".to_string()),
                 ("id2".to_string(), "state2".to_string())]
             .into_iter().collect();
-        let client = Client::new(redis::Client::open(url.as_ref()).unwrap());
         let states = client.find_all_states();
         assert_eq!(expected_states, states);
     }
