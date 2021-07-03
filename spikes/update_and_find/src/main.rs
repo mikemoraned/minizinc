d34e8bcad6d6efee2faffacbdbf7d745cb195ea0
Mike Moran <mike@houseofmoran.com>
2021-07-03 13:48:15 +0100

    - extract `find_all_states`

--- spikes/update_and_find/src/main.rs
+++ spikes/update_and_find/src/main.rs
@@ -1,12 +1,34 @@
+use std::collections::HashSet;
+
 fn main() {
     println!("Hello, world!");
+}
+
+struct Client {
+    redis_client: redis::Client
+}
+
+impl Client {
+    fn new(redis_client: redis::Client) -> Self {
+        Client { redis_client }
+    }
+
+    fn find_all_states(self) -> HashSet<(String, String)> {
+        use redis::{Commands, Iter};
+
+        let mut con = self.redis_client.get_connection().unwrap();
+
+        let states_iter: Iter<'_, (String, String)>= con.hscan("states").unwrap();
+        states_iter.collect()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use testcontainers::{clients, images, Docker};
-    use redis::{Commands, RedisResult, Iter};
+    use redis::{Commands, RedisResult};
     use std::collections::HashSet;
+    use crate::Client;
 
     #[tokio::test]
     async fn no_states_by_default() {
@@ -17,11 +39,10 @@ mod tests {
         let host_port = node.get_host_port(6379).unwrap();
         let url = format!("redis://localhost:{}", host_port);
 
-        let client = redis::Client::open(url.as_ref()).unwrap();
-        let mut con = client.get_connection().unwrap();
-
-        let size: u128 = con.hlen("states").unwrap();
-        assert_eq!(0, size);
+        let redis_client = redis::Client::open(url.as_ref()).unwrap();
+        let client = Client::new(redis_client);
+        let states = client.find_all_states();
+        assert_eq!(0, states.len());
     }
 
     #[tokio::test]
@@ -83,19 +104,19 @@ mod tests {
         let host_port = node.get_host_port(6379).unwrap();
         let url = format!("redis://localhost:{}", host_port);
 
-        let client = redis::Client::open(url.as_ref()).unwrap();
-        let mut con = client.get_connection().unwrap();
+        let redis_client = redis::Client::open(url.as_ref()).unwrap();
+        let mut con = redis_client.get_connection().unwrap();
 
         con.hset::<&str, &str, &str, u8>("states", "id1", "state1").unwrap();
         con.hset::<&str, &str, &str, u8>("states", "id2", "state2").unwrap();
 
-        let states_iter: Iter<'_, (String, String)>= con.hscan("states").unwrap();
         let expected_states : HashSet<(String, String)>
             = vec![
                 ("id1".to_string(), "state1".to_string()),
                 ("id2".to_string(), "state2".to_string())]
             .into_iter().collect();
-        let states : HashSet<(String, String)> = states_iter.collect();
+        let client = Client::new(redis::Client::open(url.as_ref()).unwrap());
+        let states = client.find_all_states();
         assert_eq!(expected_states, states);
     }
 }
