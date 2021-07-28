b2ad0f514e5af5917e458bad3c8d8d26234ea33d
Mike Moran <mike@houseofmoran.com>
2021-07-28 14:15:12 +0100

    - show `ls /` output

--- spikes/spawn_process/src/main.rs
+++ spikes/spawn_process/src/main.rs
@@ -1,3 +1,13 @@
+use std::process::Command;
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    let mut list_dir = Command::new("ls");
+
+    let output = list_dir.arg("/").output().expect("process failed to execute");
+
+    println!("stderr: {:?}, stdout: {:?}",
+             str::from_utf8(&output.stderr),
+             str::from_utf8( &output.stdout )
+    );
 }
