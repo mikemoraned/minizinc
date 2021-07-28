87e77fafa1c85121d273bd1aca51a08e12b587fe
Mike Moran <mike@houseofmoran.com>
2021-07-28 14:27:22 +0100

    - switch to web app which shows `ls /` output

--- spikes/spawn_process/src/main.rs
+++ spikes/spawn_process/src/main.rs
@@ -1,8 +1,19 @@
 use std::str;
 use tokio::process::Command;
+use actix_web::{web, App, HttpServer, Responder};
 
-#[tokio::main]
-async fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(move || {
+        App::new()
+            .route("/", web::get().to(list))
+    })
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
+}
+
+async fn list() -> impl Responder {
     let mut list_dir = Command::new("ls");
 
     let output = list_dir
@@ -11,8 +22,8 @@ async fn main() {
         .await
         .expect("process failed to execute");
 
-    println!("stderr: {:?}, stdout: {:?}",
+    format!("stderr: {:?}, stdout: {:?}",
              str::from_utf8(&output.stderr),
              str::from_utf8( &output.stdout )
-    );
+    )
 }
