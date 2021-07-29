26b7ba2ad2aac7f7643526aff8bcc48ebd479f29
Mike Moran <mike@houseofmoran.com>
2021-07-29 12:52:26 +0100

    - run on fly.io, triggered by homepage view   - note: I think this wasn't working unless I gave fairly generous grace periods / timeouts (see `services.tcp_checks` in `fly.toml`)

--- spikes/spawn_minizinc/src/main.rs
+++ spikes/spawn_minizinc/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::str;
+use tokio::process::Command;
+use actix_web::{web, App, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("starting up");
+
+    HttpServer::new(move || {
+        App::new()
+            .route("/", web::get().to(minizinc))
+    })
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
+}
+
+async fn minizinc() -> impl Responder {
+    let mut list_dir = Command::new("minizinc");
+
+    let output = list_dir
+        .arg("-D n=8")
+        .arg("--verbose")
+        .arg("nqueens.mzn")
+        .output()
+        .await
+        .expect("process failed to execute");
+
+    format!("stderr: {:?}, stdout: {:?}",
+            str::from_utf8(&output.stderr),
+            str::from_utf8( &output.stdout )
+    )
 }
