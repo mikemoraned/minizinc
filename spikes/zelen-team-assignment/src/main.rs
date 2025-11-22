5804a7eb8efb27dd530b577a3e508c52c833d24f
Mike Moran <mike@houseofmoran.com>
2025-11-22 11:27:57 +0000

    trying to parse existing team-assignment problem, but not working yet

---
+++ spikes/zelen-team-assignment/src/main.rs
@@ -0,0 +1,41 @@
+use std::fs;
+use tracing::{error, info};
+
+fn main() {
+    tracing_subscriber::fmt::init();
+
+    let mzn_path = "../../team-assignment/team-assignment.mzn";
+    let mzn_code =
+        fs::read_to_string(mzn_path).expect(format!("Failed to read {mzn_path}").as_str());
+    info!("Minizinc code read from {mzn_path}");
+
+    let dzn_path = "./team-assignment.dzn";
+    let dzn_code =
+        fs::read_to_string(dzn_path).expect(format!("Failed to read {dzn_path}").as_str());
+    info!("Minizinc code read from {dzn_path}");
+
+    let combined_code = match zelen::load_dzn_data(&dzn_code, &mzn_code) {
+        Ok(c) => {
+            info!("✓ combining successful!");
+            c
+        }
+        Err(e) => {
+            error!("✗ combining failed: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    println!("Combined Minizinc Code:\n{}", combined_code);
+
+    let ast = match zelen::parse(&combined_code) {
+        Ok(ast) => {
+            info!("✓ Parsing successful!");
+            info!("AST contains {} items", ast.items.len());
+            ast
+        }
+        Err(e) => {
+            error!("✗ Parsing failed: {}", e);
+            std::process::exit(1);
+        }
+    };
+}
